e6560e1cb713626d6ca4f99513c72fc2bf6d9a8e
dzamarrahim <dzamarrahim@gmail.com>
2024-11-22 10:39:53 +0700

    Memperbaiki Tampilan Backend

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/welcome', function () {
+    return view('welcome');
+});
 
 Route::get('/', [\App\Http\Controllers\HomeController::class, 'spots']);
 Route::get('/detail-spot/{slug}',[\App\Http\Controllers\HomeController::class,'detailSpot'])->name('detail-spot');
