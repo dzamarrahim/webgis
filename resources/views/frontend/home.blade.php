3c3e67af4e7bc9e259a7dbd62e07876e8b54dc53
dzamarrahim <dzamarrahim@gmail.com>
2025-01-04 13:42:50 +0700

    Menambahkan Animasi

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -6,6 +6,16 @@
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/leaflet-search@3.0.9/dist/leaflet-search.src.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/leaflet.fullscreen@2.4.0/Control.FullScreen.min.css">
+
+    <!-- AOS -->
+    <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
+
+    <!-- Animate Style -->
+    <link
+    rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
+  />
+
     <style>
         .hero {
             background-image: linear-gradient(rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.8)),
@@ -76,8 +86,8 @@
         <div class="container">
             <div class="row">
                 <div class="col text-center">
-                    <h1 class="text-white mb-4 fw-bold">Selamat Datang <br> WebGIS PUPR Aceh Tamiang</h1>
-                    <p class="text-white mb-4 text-opacity-75">Menampilkan Informasi Letak Geografis, Batas Administrasi Kecamatan di Kabupaten Aceh Tamiang</p>
+                    <h1 class="text-white mb-4 fw-bold animate__animated animate__zoomIn">Selamat Datang <br> WebGIS PUPR Aceh Tamiang</h1>
+                    <p class="text-white mb-4 text-opacity-75"></p>
                 </div>
             </div>
         </div>
@@ -107,12 +117,12 @@
       <div class="latar" id="latar">
         <div class="container my-5">
             <div class="row">
-                <div class="col">
+                <div class="col"  data-aos="fade-right" data-aos-duration="1000" data-tilt data-tilt-scale="1.1">
                     <img src="{{ asset('icon/peta-aceh.jpg')}}" alt="">
                 </div>
                 <div class="col">
-                    <h2 class="mb-4 display-4 fw-semibold">Latar Belakang</h2>
-                    <p class="fs-5">Dinas Pekerjaan Umum dan Penataan Ruang (PUPR) mempunyai peran penting dalam penyusunan kebijakan pembangunan daerah dengan mengacu pada analisa data dan asumsi pertumbuhan daerah pada tahun rencana. Salah satu data yang dibutuhkan dalam perencanaan kebijakan pembangunan baik secara makro dan sektoral adalah data geospasial yang dikelola oleh Bidang Tata Ruang Dinas Pekerjaan Umum dan Penataan Ruang Kabupaten Aceh Tamiang.</p>
+                    <h2 class="mb-4 display-4 fw-semibold" data-aos="fade-left" data-aos-duration="1000" data-aos-delay="500">Latar Belakang</h2>
+                    <p class="fs-5" data-aos="fade-left" data-aos-duration="1000" data-aos-delay="1000">Dinas Pekerjaan Umum dan Penataan Ruang (PUPR) mempunyai peran penting dalam penyusunan kebijakan pembangunan daerah dengan mengacu pada analisa data dan asumsi pertumbuhan daerah pada tahun rencana. Salah satu data yang dibutuhkan dalam perencanaan kebijakan pembangunan baik secara makro dan sektoral adalah data geospasial yang dikelola oleh Bidang Tata Ruang Dinas Pekerjaan Umum dan Penataan Ruang Kabupaten Aceh Tamiang.</p>
                 </div>
             </div>
         </div>
@@ -124,12 +134,12 @@
         <div class="container my-5">
             <div class="row">
                 <div class="col">
-                    <h2 class="mb-4 display-4 fw-semibold">Apa Itu GIS?</h2>
-                    <p class="fs-5 mb-4">Secara umum Geographic Information System (GIS) juga dikenal sebagai Sistem Informasi Geografis (SIG) di Indonesia</p>
-                    <p class="fs-5 fst-italic mb-4">“GIS adalah suatu komponen yang terdiri dari perangkat keras, perangkat lunak, data geografis dan sumber daya manusia yang bekerja bersama secara efektif untuk memasukan, menyimpan, memperbaiki, memperbaharui, mengelola, memanipulasi, mengintegrasikan, menganalisa dan menampilkan data dalam suatu informasi berbasis geografis“</p>
-                    <p class="">Sumber: www.geospasialis.com & YouTube channel “ESRI”</p>
+                    <h2 class="mb-4 display-4 fw-semibold" data-aos="fade-right" data-aos-duration="1000">Apa Itu GIS?</h2>
+                    <p class="fs-5 mb-4" data-aos="fade-right" data-aos-duration="1000" data-aos-delay="500">Secara umum Geographic Information System (GIS) juga dikenal sebagai Sistem Informasi Geografis (SIG) di Indonesia</p>
+                    <p class="fs-5 fst-italic mb-4" data-aos="fade-right" data-aos-duration="1000" data-aos-delay="1000">“GIS adalah suatu komponen yang terdiri dari perangkat keras, perangkat lunak, data geografis dan sumber daya manusia yang bekerja bersama secara efektif untuk memasukan, menyimpan, memperbaiki, memperbaharui, mengelola, memanipulasi, mengintegrasikan, menganalisa dan menampilkan data dalam suatu informasi berbasis geografis“</p>
+                    <p class="" data-aos="fade-right" data-aos-duration="1000">Sumber: www.geospasialis.com & YouTube channel “ESRI”</p>
                 </div>
-                <div class="col text-center">
+                <div class="col text-center" data-aos="fade-left" data-aos-duration="1000" data-aos-delay="2000">
                     <img src="{{ asset('icon/maps-icon.png') }}" alt="">
                 </div>
             </div>
@@ -141,13 +151,13 @@
       <div class="webgis bg-dark" id="webgis">
         <div class="container py-5">
             <div class="row py-5">
-                <div class="col">
-                    <img src="{{ asset('icon/webgis.png') }}" alt="" class="">
+                <div class="col" data-aos="fade-right" data-aos-duration="1000">
+                    <img src="{{ asset('icon/gis.png') }}" alt="" class="">
                 </div>
                 <div class="col ms-4">
-                    <h3 class="text-white mb-4">WebGIS Kabupaten Aceh Tamiang</h3>
-                    <p class="text-white mb-4">WebGIS adalah pemetaan digital yang memanfaatkan jaringan internet sebagai media komunikasi yang berfungsi mendistribusikan, mempublikasikan, mengintegrasikan, mengkomunikasikan dan menyediakan informasi dalam bentuk teks, peta dijital serta menjalankan fungsi–fungsi analisis dan query yang terkait dengan GIS. Untuk mengakses WebGIS Kabupaten Aceh Tamiang silahkan klik link berikut</p>
-                    <a href="{{ route('webgis') }}" class="btn btn-success btn-lg px-4 py-2 rounded-1" target="_blank">WebGIS Aceh Tamiang</a>
+                    <h3 class="text-white mb-4" data-aos="fade-left" data-aos-duration="1000" data-aos-delay="500">WebGIS Kabupaten Aceh Tamiang</h3>
+                    <p class="text-white mb-4" data-aos="fade-left" data-aos-duration="1000" data-aos-delay="1000">WebGIS adalah pemetaan digital yang memanfaatkan jaringan internet sebagai media komunikasi yang berfungsi mendistribusikan, mempublikasikan, mengintegrasikan, mengkomunikasikan dan menyediakan informasi dalam bentuk teks, peta digital serta menjalankan fungsi–fungsi analisis dan query yang terkait dengan GIS. Untuk mengakses WebGIS Kabupaten Aceh Tamiang silahkan klik link berikut</p>
+                    <a href="{{ route('webgis') }}" class="btn btn-success btn-lg px-4 py-2 rounded-1" target="_blank" data-aos="zoom-in" data-aos-duration="1000" data-aos-delay="500">WebGIS Aceh Tamiang</a>
                 </div>
             </div>
         </div>
@@ -158,12 +168,12 @@
       <div class="faq" id="faq">
         <div class="container">
             <div class="row">
-                <div class="col">
+                <div class="col" data-aos="fade-right" data-aos-duration="1000">
                     <h2 class="fw-semibold">Pertanyaan Umum <br> Yang Sering ditanyakan</h2>
                 </div>
             </div>
             <div class="row row-cols-lg-2 row-cols-1 g-4 pt-4">
-                <div class="col">
+                <div class="col" data-aos="zoom-in" data-aos-duration="1000" data-aos-delay="300">
                     <div class="accordion" id="accordionExample">
                         <div class="accordion-item">
                             <h2 class="accordion-header">
@@ -179,7 +189,7 @@
                         </div>
                     </div>
                 </div>
-                <div class="col">
+                <div class="col" data-aos="zoom-in" data-aos-duration="1000" data-aos-delay="300">
                     <div class="accordion" id="accordionExample">
                         <div class="accordion-item">
                             <h2 class="accordion-header">
@@ -195,7 +205,7 @@
                         </div>
                     </div>
                 </div>
-                <div class="col">
+                <div class="col" data-aos="zoom-in" data-aos-duration="1000" data-aos-delay="600">
                     <div class="accordion" id="accordionExample">
                         <div class="accordion-item">
                             <h2 class="accordion-header">
@@ -211,7 +221,7 @@
                         </div>
                     </div>
                 </div>
-                <div class="col">
+                <div class="col" data-aos="zoom-in" data-aos-duration="1000" data-aos-delay="600">
                     <div class="accordion" id="accordionExample">
                         <div class="accordion-item">
                             <h2 class="accordion-header">
@@ -231,3 +241,31 @@
         </div>
       </div>
 @endsection
+
+@push('javascript')
+<script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+  <script>
+    AOS.init();
+  </script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/vanilla-tilt/1.7.0/vanilla-tilt.min.js"></script>
+<script>
+    VanillaTilt.init(document.querySelectorAll(".latar img"), {
+      max: 25,
+      speed: 400,
+      glare: true,
+      "max-glare": 0.5,
+    });
+</script>
+
+<script src="https://cdn.jsdelivr.net/npm/gsap@3.12.5/dist/gsap.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/gsap@3.12.5/dist/TextPlugin.min.js"></script>
+<script>
+    gsap.registerPlugin(TextPlugin)
+    gsap.to('.hero p', {
+        duration: 2,
+        delay: 0.8,
+        text: "Menampilkan Informasi Letak Geografis, Batas Administrasi Kecamatan di Kabupaten Aceh Tamiang",
+        ease: "none",
+    });
+</script>
+@endpush
